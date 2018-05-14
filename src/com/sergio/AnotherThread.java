ccf0bda41e715d89ecd0410590c70e8242df2082
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-14 22:37:39 +0100

    Threads

---
+++ src/com/sergio/AnotherThread.java
@@ -0,0 +1,10 @@
+package com.sergio;
+
+public class AnotherThread extends Thread{
+
+	@Override
+	public void run() {
+		System.out.println("Hello from another thread.");
+	}
+
+}
---
+++ src/com/sergio/Main.java
@@ -0,0 +1,22 @@
+package com.sergio;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello from the main thread");
+		
+		Thread anotherThread = new AnotherThread();
+		anotherThread.start();
+
+		new Thread() {
+			public void run() {
+				System.out.println("Hello from the anonymous class thread");
+			}
+		}.start();
+		
+		System.out.println("Hello again from the main thread.");
+		
+	}
+
+}
